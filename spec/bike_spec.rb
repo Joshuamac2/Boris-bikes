0f53a0b371b483dd6c73d904d676ab034201cf32
Joshua <joshuamacleod@live.com>
2020-12-06 19:32:28 +0000

    Test to only release bike when avilable passes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,9 +5,4 @@ describe Bike do
   it 'checks bike is working' do
     expect(subject).to respond_to :working?
   end
-
-  it 'responds to bike' do
-    expect(subject).to respond_to :bike
-  end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it 'respondes to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
     bike = subject.release_bike
@@ -15,6 +13,8 @@ describe DockingStation do
     expect(subject).to respond_to(:dock).with(1).argument
   end
 
+  it { is_expected.to respond_to(:bike) }
+
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
@@ -24,6 +24,18 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes abilable' do
+      expect { subject.release_bike }.to raise_error 'No bikes avilable'
+    end
   end
 
 end
